272514b2c627abdbb6980ce480bd623d9e7afb84
unknown <meryembenchelh@gmail.com>
2025-01-04 00:53:50 +0100

    question1

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+const API_URL = 'https://dummyjson.com/posts';
 // Fonction pour récupérer et afficher les derniers articles
 async function fetchLatestNews() {
     try {
@@ -13,7 +14,22 @@ async function fetchLatestNews() {
 // TODO: Question 1 - Compléter la fonction displayNews
 function displayNews(news) {
     const container = document.getElementById('news-container');
-    // Utilisez Bootstrap pour créer des cards pour chaque article
+    newsContainer.innerHTML = ''; // Reset the container before adding new articles
+    
+    articles.forEach(article => {
+        const articleCard = `
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">${article.title}</h5>
+                        <p class="card-text">${article.body.substring(0, 100)}...</p>
+                        <button class="btn btn-primary" onclick="viewArticle(${article.id})">Read More</button>
+                    </div>
+                </div>
+            </div>
+        `;
+        newsContainer.innerHTML += articleCard;
+    });
 }
 
 // TODO: Question 2 - Créer une fonction pour gérer les erreurs
